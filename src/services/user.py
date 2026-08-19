385190b0d03956296fede8b00c48468a40e829c2
Offensive94 <montagemymy@gmail.com>
2026-08-20 02:46:06 +0500

    форматирование сервисов user и verify

--- src/services/user.py
+++ src/services/user.py
@@ -19,6 +19,7 @@ logger = logging.getLogger(__name__)
 
 
 class IUserService(ABC):
+
     @abstractmethod
     async def create(self, user_data: UserCreateDTO) -> User: ...
 
@@ -32,24 +33,37 @@ class UserService(IUserService):
 
     async def create(self, user_data: UserCreateDTO) -> User:
         async with self._uow as uow:
-            if await uow.user_repository.get_by_email(user_email=user_data.email):
+            if await uow.user_repository.get_by_email(
+                user_email=user_data.email
+            ):
                 raise UserEmailAlreadyExists()
 
-            redis_data = await uow.verify_repository.get_value(user_data.verify_token)
+            redis_data = await uow.verify_repository.get_value(
+                user_data.verify_token
+            )
 
-            if not redis_data or json.loads(redis_data)["status"] != "verified":
+            if (
+                not redis_data or
+                json.loads(redis_data)["status"] != "verified"
+            ):
                 raise VerifyCodeNotConfirmed()
 
             await uow.verify_repository.delete_value(user_data.verify_token)
 
-            if await uow.user_repository.get_by_username(username=user_data.username):
+            if await uow.user_repository.get_by_username(
+                username=user_data.username
+            ):
                 raise UsernameAlreadyExists()
 
             hashed_password = password_hasher.hash(user_data.password).encode("utf-8")
             user_dict = user_data.model_dump(exclude={"password"})
-            user_data = UserCreateDatabaseDTO(password_hash=hashed_password, **user_dict)
+            user_data = UserCreateDatabaseDTO(
+                password_hash=hashed_password,
+                **user_dict
+            )
             return await uow.user_repository.create(user_data)
 
     async def get_all(self) -> list[User]:
         async with self._uow as uow:
             return await uow.user_repository.get_all()
+
--- src/services/verify.py
+++ src/services/verify.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 
 
 class IVerifyService(ABC):
+
     @abstractmethod
     async def create_email_code(self, user_email: EmailStr) -> str: ...
 
@@ -34,29 +35,43 @@ class VerifyService(IVerifyService):
             verification_code = 666_666
             verify_token = secrets.token_urlsafe(32)
 
-            redis_data = {"email": user_email, "code": verification_code, "status": "pending"}
+            redis_data = {
+                "email": user_email,
+                "code": verification_code,
+                "status": "pending"
+            }
 
             await uow.verify_repository.set_value(
-                key=verify_token, value=json.dumps(redis_data), exp=5
+                key=verify_token,
+                value=json.dumps(redis_data),
+                exp=5
             )
 
             return verify_token
 
     async def verify_email_code(self, verify_data: VerifyCodeDTO):
         async with self._uow as uow:
-            if await uow.user_repository.get_by_email(user_email=verify_data.email):
+            if await uow.user_repository.get_by_email(
+                user_email=verify_data.email
+            ):
                 raise UserEmailAlreadyExists()
 
-            raw_redis_data = await uow.verify_repository.get_value(verify_data.verify_token)
+            raw_redis_data = await uow.verify_repository.get_value(
+                verify_data.verify_token
+            )
             if not raw_redis_data:
                 raise CodeHasExpired()
 
             redis_data = json.loads(raw_redis_data)
-            if not (
-                redis_data["email"] == verify_data.email and redis_data["code"] == verify_data.code
+            if not(
+                redis_data["email"] == verify_data.email
+                and redis_data["code"] == verify_data.code
             ):
                 raise InvalidVerifyCode()
 
             await uow.verify_repository.update_field(
-                key=verify_data.verify_token, field="status", value="verified", new_exp=24 * 60
+                key=verify_data.verify_token,
+                field="status",
+                value="verified",
+                new_exp=24 * 60
             )
