63e0d50a01a1e01ee4e08333c097c8d7585b7a4c
Offensive94 <montagemymy@gmail.com>
2026-08-20 23:07:17 +0500

    запроса мокка

--- tests/unit/infrastructure/test_exception_handlers.py
+++ tests/unit/infrastructure/test_exception_handlers.py
@@ -12,53 +12,54 @@ from src.domain.exceptions import (
 from src.infrastructure.handlers.exceptions import exception_handlers
 
 
+@pytest.fixture
+def mock_request():
+    return AsyncMock(spec=Request)
+
+
 @pytest.mark.asyncio
-async def test_exception_handler_user_not_found():
+async def test_exception_handler_user_not_found(mock_request):
     handler = exception_handlers[UserNotFound]
-    request = AsyncMock(spec=Request)
     exc = UserNotFound()
 
     with pytest.raises(HTTPException) as exc_info:
-        await handler(request, exc)
+        await handler(mock_request, exc)
 
     assert exc_info.value.status_code == 404
     assert exc_info.value.detail == "Пользователь не найден."
 
 
 @pytest.mark.asyncio
-async def test_exception_handler_email_already_exists():
+async def test_exception_handler_email_already_exists(mock_request):
     handler = exception_handlers[UserEmailAlreadyExists]
-    request = AsyncMock(spec=Request)
     exc = UserEmailAlreadyExists()
 
     with pytest.raises(HTTPException) as exc_info:
-        await handler(request, exc)
+        await handler(mock_request, exc)
 
     assert exc_info.value.status_code == 409
     assert exc_info.value.detail == "Пользователь с таким email уже существует."
 
 
 @pytest.mark.asyncio
-async def test_exception_handler_username_already_exists():
+async def test_exception_handler_username_already_exists(mock_request):
     handler = exception_handlers[UsernameAlreadyExists]
-    request = AsyncMock(spec=Request)
     exc = UsernameAlreadyExists()
 
     with pytest.raises(HTTPException) as exc_info:
-        await handler(request, exc)
+        await handler(mock_request, exc)
 
     assert exc_info.value.status_code == 409
     assert exc_info.value.detail == "Пользователь с таким username уже существует."
 
 
 @pytest.mark.asyncio
-async def test_exception_handler_verify_code_not_confirmed():
+async def test_exception_handler_verify_code_not_confirmed(mock_request):
     handler = exception_handlers[VerifyCodeNotConfirmed]
-    request = AsyncMock(spec=Request)
     exc = VerifyCodeNotConfirmed()
 
     with pytest.raises(HTTPException) as exc_info:
-        await handler(request, exc)
+        await handler(mock_request, exc)
 
     assert exc_info.value.status_code == 400
     assert exc_info.value.detail == "Проверочный код не подтвержден."
