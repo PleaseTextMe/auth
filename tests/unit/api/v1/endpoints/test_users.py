f48f251310eccc373dd14b0d2c8e582a50f013a2
Offensive94 <montagemymy@gmail.com>
2026-09-14 22:28:45 +0500

    перевел тесты пользователей на общую фикстуру test_app

--- tests/unit/api/v1/endpoints/test_users.py
+++ tests/unit/api/v1/endpoints/test_users.py
@@ -1,26 +1,4 @@
 import pytest
-from dishka import Provider, Scope, make_async_container, provide
-from dishka.integrations.fastapi import setup_dishka
-from fastapi import FastAPI
-
-from src.api.v1.router import router
-from src.services.user import IUserService
-
-
-@pytest.fixture
-async def test_app(mock_user_service):
-    app = FastAPI()
-    app.include_router(router, prefix="/api")
-
-    class MockProvider(Provider):
-        @provide(scope=Scope.APP)
-        def get_user_service(self) -> IUserService:
-            return mock_user_service
-
-    container = make_async_container(MockProvider())
-    setup_dishka(container=container, app=app)
-    yield app
-    await container.close()
 
 
 @pytest.mark.asyncio
