86c00f8b9728b1ec7a1cf9a6c9ac0b78a09cd1c5
Offensive94 <montagemymy@gmail.com>
2026-09-16 22:04:15 +0500

    перевел тесты зависимостей на универсальный test_app

--- tests/unit/api/v1/test_depends.py
+++ tests/unit/api/v1/test_depends.py
@@ -1,36 +1,22 @@
-
 import pytest
-from dishka import Provider, Scope, make_async_container, provide
-from dishka.integrations.fastapi import setup_dishka
-from fastapi import Depends, FastAPI
+from fastapi import Depends
 
 from src.api.v1.depends import get_current_session, get_current_user
 from src.domain.entities.session import Session
 from src.domain.entities.user import User
-from src.services.interfaces.uow import IUnitOfWork
 
 
-@pytest.fixture
-async def test_app(mock_uow):
-    app = FastAPI()
+@pytest.fixture(autouse=True)
+def setup_depends_routes(test_app):
+    test_app.dependency_overrides.clear()
 
-    @app.get("/session/")
+    @test_app.get("/session/")
     async def get_session(session: Session = Depends(get_current_session)):
         return {"id": session.id}
 
-    @app.get("/user/")
+    @test_app.get("/user/")
     async def get_user(user: User = Depends(get_current_user)):
         return {"id": user.id}
-
-    class MockProvider(Provider):
-        @provide(scope=Scope.APP)
-        def get_uow(self) -> IUnitOfWork:
-            return mock_uow
-
-    container = make_async_container(MockProvider())
-    setup_dishka(container=container, app=app)
-    yield app
-    await container.close()
 
 
 @pytest.mark.asyncio
